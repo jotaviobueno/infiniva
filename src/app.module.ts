5dd799bc6eb57fe5ed9178e7ee732b7ddc179f42
unknown <joseotaviocbueno@gmail.com>
2023-04-14 15:42:17 -0300

    chore: adicionado novas importações

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { MongodbModule } from './repositories/implementations/mongodb/mongodb.module';
 import { UserModule } from './modules/user/user.module';
+import { PaginationModule } from './modules/pagination/pagination.module';
 
 @Module({
-  imports: [MongodbModule, UserModule],
+  imports: [MongodbModule, UserModule, PaginationModule],
   controllers: [],
   providers: [],
 })
