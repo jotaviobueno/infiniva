0a0b6300470364eabe9f5362f581d5e72e018aa2
unknown <joseotaviocbueno@gmail.com>
2023-04-14 21:28:58 -0300

    feat: novas importações adicionada

--- src/repositories/implementations/mongodb/mongodb.module.ts
+++ src/repositories/implementations/mongodb/mongodb.module.ts
@@ -2,22 +2,33 @@ import { Module } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 import { MONGO_URI } from 'src/config/mongodb';
 
-import { User, UserSchema } from './schemas/user';
+import { User, UserSchema } from './schemas/user.schema';
+import { Auth, AuthSchema } from './schemas/auth.schema';
 
 import { MongodbUserRepository } from './user/mongodb-user.repository';
 import { UserRepository } from 'src/repositories/abstracts/user/user.repository';
 
+import { AuthRepository } from 'src/repositories/abstracts/auth/auth.repository';
+import { MongodbAuthRepository } from './auth/mongodb-auth.repository';
+
 @Module({
   imports: [
     MongooseModule.forRoot(MONGO_URI),
-    MongooseModule.forFeature([{ name: User.name, schema: UserSchema }]),
+    MongooseModule.forFeature([
+      { name: User.name, schema: UserSchema },
+      { name: Auth.name, schema: AuthSchema },
+    ]),
   ],
   providers: [
     {
       provide: UserRepository,
       useClass: MongodbUserRepository,
     },
+    {
+      provide: AuthRepository,
+      useClass: MongodbAuthRepository,
+    },
   ],
-  exports: [UserRepository],
+  exports: [UserRepository, AuthRepository],
 })
 export class MongodbModule {}
