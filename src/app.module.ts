fa6e6c4233e12af2234ac82b691ae76213f3a1d1
unknown <joseotaviocbueno@gmail.com>
2023-04-14 21:30:13 -0300

    feat: novas importações

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { MongodbModule } from './repositories/implementations/mongodb/mongodb.module';
 import { UserModule } from './modules/user/user.module';
 import { PaginationModule } from './modules/pagination/pagination.module';
+import { AuthModule } from './modules/auth/auth.module';
 
 @Module({
-  imports: [MongodbModule, UserModule, PaginationModule],
+  imports: [MongodbModule, UserModule, PaginationModule, AuthModule],
   controllers: [],
   providers: [],
 })
