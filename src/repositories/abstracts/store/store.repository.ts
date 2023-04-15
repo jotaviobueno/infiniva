e26152044e7b426cb515f54eb7d7270787fe89de
unknown <joseotaviocbueno@gmail.com>
2023-04-15 20:39:22 -0300

    feat: novas funções adicionada

--- src/repositories/abstracts/store/store.repository.ts
+++ src/repositories/abstracts/store/store.repository.ts
@@ -1,5 +1,9 @@
+import { UpdateResult } from 'mongodb';
 import { Types } from 'mongoose';
+import { IOffsetAndLimit } from 'src/modules/pagination/interfaces/ioffset-and-limit';
 import { CreateStoreDto } from 'src/modules/store/dto/create-store.dto';
+import { UpdateStoreDto } from 'src/modules/store/dto/update-store.dto';
+import { IStores } from 'src/modules/store/interfaces/istores';
 import { Store } from 'src/repositories/implementations/mongodb/schemas/store.schema';
 
 export abstract class StoreRepository {
@@ -7,6 +11,28 @@ export abstract class StoreRepository {
     userId: Types.ObjectId,
     createStoreDto: CreateStoreDto,
   ): Promise<Store>;
+
+  abstract findByStoreId(store_id: string): Promise<Store>;
+
+  abstract findById(storeId: Types.ObjectId): Promise<Store>;
+
+  abstract findByName(name: string): Promise<Store>;
+
+  abstract findAll(offsetAndLimit: IOffsetAndLimit): Promise<IStores[]>;
+
+  abstract findByUserId(userId: Types.ObjectId): Promise<Store[]>;
+
+  abstract searchByName(
+    name: string,
+    offsetAndLimit: IOffsetAndLimit,
+  ): Promise<IStores[]>;
+
+  abstract update(
+    storeId: Types.ObjectId,
+    updateStoreDto: UpdateStoreDto,
+  ): Promise<UpdateResult>;
+
+  abstract remove(storeId: Types.ObjectId): Promise<UpdateResult>;
 
   abstract countingAllStoresTheUser(userId: Types.ObjectId): Promise<number>;
 }
