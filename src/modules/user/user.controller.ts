4cc53375be29e1ece146e8de042aa9aaf019452f
unknown <joseotaviocbueno@gmail.com>
2023-04-14 21:38:43 -0300

    feat: nescessario estar logado para acessar as rotas privadas

--- src/modules/user/user.controller.ts
+++ src/modules/user/user.controller.ts
@@ -7,12 +7,14 @@ import {
   Param,
   Delete,
   Query,
+  UseGuards,
 } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
 import { ReqOffsetAndLimit } from '../pagination/decorators/req-offset-and-limit';
 import { IOffsetAndLimit } from '../pagination/interfaces/ioffset-and-limit';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('user')
 export class UserController {
@@ -24,6 +26,7 @@ export class UserController {
   }
 
   @Get('search')
+  @UseGuards(AuthGuard('jwt'))
   searchByName(
     @Query('name') name: string,
     @ReqOffsetAndLimit() offsetAndLimit: IOffsetAndLimit,
@@ -32,16 +35,19 @@ export class UserController {
   }
 
   @Get('/a/:account_id')
+  @UseGuards(AuthGuard('jwt'))
   findByAccountId(@Param('account_id') account_id: string) {
     return this.userService.findByAccountId(account_id);
   }
 
   @Get(':username')
+  @UseGuards(AuthGuard('jwt'))
   findByUsername(@Param('username') username: string) {
     return this.userService.findByUsername(username);
   }
 
   @Patch(':id')
+  @UseGuards(AuthGuard('jwt'))
   update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
     return this.userService.update(+id, updateUserDto);
   }
