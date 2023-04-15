eda69975f1a93fe1814c2e6d5d99af81f74af309
unknown <joseotaviocbueno@gmail.com>
2023-04-15 02:44:46 -0300

    feat: adicionado novas funções

--- src/repositories/implementations/mongodb/auth/mongodb-auth.repository.ts
+++ src/repositories/implementations/mongodb/auth/mongodb-auth.repository.ts
@@ -3,6 +3,8 @@ import { InjectModel } from '@nestjs/mongoose';
 import { AuthRepository } from 'src/repositories/abstracts/auth/auth.repository';
 import { Auth, AuthDocument } from '../schemas/auth.schema';
 import { Model, Types } from 'mongoose';
+import { UpdateResult } from 'mongodb';
+import { IOffsetAndLimit } from 'src/modules/pagination/interfaces/ioffset-and-limit';
 
 @Injectable()
 export class MongodbAuthRepository implements AuthRepository {
@@ -24,5 +26,43 @@ export class MongodbAuthRepository implements AuthRepository {
 
   async findByAccessId(access_id: string): Promise<Auth> {
     return await this.model.findOne({ access_id, disconnectedAt: null });
+  }
+
+  async findAll(
+    userId: Types.ObjectId,
+    { limit, offset }: IOffsetAndLimit,
+  ): Promise<Auth[]> {
+    const pagination = [{ $skip: offset }, { $limit: limit }];
+    const pipeline = [
+      {
+        $match: {
+          userId,
+          disconenctAt: null,
+        },
+      },
+      {
+        $project: {
+          _id: 0,
+          userId: 0,
+          __v: 0,
+        },
+      },
+    ];
+
+    return await this.model.aggregate([...pipeline, ...pagination]);
+  }
+
+  async disconnect(access_id: string): Promise<UpdateResult> {
+    return await this.model.updateOne(
+      { access_id },
+      { disconnectedAt: new Date(), updatedAt: new Date() },
+    );
+  }
+
+  async disconnectMany(userId: Types.ObjectId): Promise<UpdateResult> {
+    return await this.model.updateMany(
+      { userId, disconnectedAt: null },
+      { disconnectedAt: new Date(), updatedAt: new Date() },
+    );
   }
 }
