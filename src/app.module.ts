1497b20d594e11c59ed520919b9eb6bbfc113dd6
unknown <joseotaviocbueno@gmail.com>
2023-04-15 21:55:31 -0300

    feat: novas importações

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,6 +4,8 @@ import { UserModule } from './modules/user/user.module';
 import { PaginationModule } from './modules/pagination/pagination.module';
 import { AuthModule } from './modules/auth/auth.module';
 import { StoreModule } from './modules/store/store.module';
+import { ProductModule } from './modules/product/product.module';
+import { CategoryModule } from './modules/category/category.module';
 
 @Module({
   imports: [
@@ -12,6 +14,8 @@ import { StoreModule } from './modules/store/store.module';
     PaginationModule,
     AuthModule,
     StoreModule,
+    ProductModule,
+    CategoryModule,
   ],
   controllers: [],
   providers: [],
