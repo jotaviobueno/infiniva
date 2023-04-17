2563f58a43771f4f60e293493441efbb579025d9
unknown <joseotaviocbueno@gmail.com>
2023-04-16 21:27:49 -0300

    feat: novas importações adicionada

--- src/repositories/implementations/mongodb/mongodb.module.ts
+++ src/repositories/implementations/mongodb/mongodb.module.ts
@@ -6,6 +6,8 @@ import { MONGO_URI } from 'src/config/mongodb';
 import { User, UserSchema } from './schemas/user.schema';
 import { Auth, AuthSchema } from './schemas/auth.schema';
 import { Store, StoreSchema } from './schemas/store.schema';
+import { Product, ProductSchema } from './schemas/product.schema';
+import { Category, CategorySchema } from './schemas/category.schema';
 
 // Repositories
 import { MongodbUserRepository } from './user/mongodb-user.repository';
@@ -17,6 +19,12 @@ import { MongodbAuthRepository } from './auth/mongodb-auth.repository';
 import { StoreRepository } from 'src/repositories/abstracts/store/store.repository';
 import { MongodbStoreRepository } from './store/mongodb-store.repository';
 
+import { ProductRepository } from 'src/repositories/abstracts/product/product.repository';
+import { MongodbProductRepository } from './product/mongodb-product.repository';
+
+import { CategoryRepository } from 'src/repositories/abstracts/category/category.repository';
+import { MongodbCategoryRepository } from './category/mongodb-category.repository';
+
 @Module({
   imports: [
     MongooseModule.forRoot(MONGO_URI),
@@ -24,6 +32,8 @@ import { MongodbStoreRepository } from './store/mongodb-store.repository';
       { name: User.name, schema: UserSchema },
       { name: Auth.name, schema: AuthSchema },
       { name: Store.name, schema: StoreSchema },
+      { name: Product.name, schema: ProductSchema },
+      { name: Category.name, schema: CategorySchema },
     ]),
   ],
   providers: [
@@ -39,7 +49,25 @@ import { MongodbStoreRepository } from './store/mongodb-store.repository';
       provide: StoreRepository,
       useClass: MongodbStoreRepository,
     },
+    {
+      provide: ProductRepository,
+      useClass: MongodbProductRepository,
+    },
+    {
+      provide: ProductRepository,
+      useClass: MongodbProductRepository,
+    },
+    {
+      provide: CategoryRepository,
+      useClass: MongodbCategoryRepository,
+    },
   ],
-  exports: [UserRepository, AuthRepository, StoreRepository],
+  exports: [
+    UserRepository,
+    AuthRepository,
+    StoreRepository,
+    ProductRepository,
+    CategoryRepository,
+  ],
 })
 export class MongodbModule {}
